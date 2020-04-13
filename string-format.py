e83837a947ac4c137a162e3dc5148c203f79ec01
Anterra Kennedy <anterrakennedy@gmail.com>
2020-04-12 23:56:55 -0700

    new project

--- string-format.py
+++ string-format.py
@@ -9,18 +9,18 @@ name + " is " + str(age) + " years old"
 
 #same output, other options:
 print("{} was {} years old when she learned python".format(name, age))
-print('{name} was {age} years old when she learned python'.format(name=name, age=age))
+print("{name} was {age} years old when she learned python".format(name=name, age=age))
 
 #f-strings, quicker
-print(f'{name} was {age} years old when she learned python')
+print(f"{name} was {age} years old when she learned python")
 
 # decimal (.) precision of 3 for float '0.333'
-print('{0:.3f}'.format(1.0/3))
+print("{0:.3f}".format(1.0/3))
 
 # fill with underscores (_) with the text centered
 # (^) to 11 width '___hello___'
-print('{0:_^11}'.format('hello'))
+print("{0:_^11}".format("hello"))
 
 # keyword-based 'Swaroop wrote A Byte of Python'
-print('{name} wrote {book}'.format(name='Swaroop', book='A Byte of Python'))
+print("{name} wrote {book}".format(name="Swaroop", book="A Byte of Python"))
 
