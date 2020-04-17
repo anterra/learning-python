d8715cf30b48d525b21273f132152f4605dd4f27
Anterra Kennedy <anterrakennedy@gmail.com>
2020-04-17 14:06:36 -0700

    april 17th 2pm

---
+++ problem_solving/backup_ver3.py
@@ -0,0 +1,53 @@
+#my first own program! no demo
+
+#same program to backup important files
+#but this time using the built in 'zipfile' module (part of standard library)
+#so that program can be executed inside Pycharm
+#not relying on os.system
+#a solution to first 2 versions' problem!
+#also preferable because this way program is self contained, not reliant on system's zip capabilities
+
+
+import os
+import time
+from zipfile import ZipFile
+
+source = [r"C:\Users\Anterra\FilesToBackUp"]
+
+target_dir = r"C:\Users\Anterra\BackUp"
+
+if not os.path.exists(target_dir):
+    os.mkdir(target_dir)
+
+today = target_dir + os.sep + time.strftime("%Y%m%d")
+now = time.strftime("%H%M%S")
+
+comment = input("Enter a comment --> ")
+if len(comment) == 0:
+    target = today + os.sep + now + ".zip"
+else:
+    target = today + os.sep + now + "_" + comment.replace(" ", "_") + ".zip"
+
+if not os.path.exists(today):
+    os.mkdir(today)
+    print("Successfully created directory", today)
+
+#a solution found on the internet:
+def get_file_paths(directory):
+    file_paths = []
+    for root, directories, files in os.walk(directory):
+        for filename in files:
+            filepath = os.path.join(root, filename)
+            file_paths.append(filepath)
+    return file_paths
+
+with ZipFile(target, "w") as zip: #w = write mode
+    print("{} is created".format(target))
+    print("These files will be zipped:")
+    file_paths = get_file_paths(" ".join(source))
+    for file_name in file_paths:
+        print(file_name)
+    for file in file_paths:
+        zip.write(file)
+    print("Files backed up")
+
